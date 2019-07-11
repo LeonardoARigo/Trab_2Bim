af69bf057a7b28d288c471c42c69e05302c581b2
LeonardoARigo <leonardorigo76@gmail.com>
2019-07-11 12:38:16 -0300

    a

--- app/src/main/java/com/example/trab_2bim/Main2Activity.java
+++ app/src/main/java/com/example/trab_2bim/Main2Activity.java
@@ -5,36 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+    Bundle pacote = new Bundle();
+    Intent intent;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        intent = new Intent(this, Main3Activity.class);
+        Spinner spinner = (Spinner)findViewById(R.id.spinner1);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                Spinner spinner = (Spinner)findViewById(R.id.spinner1);
+                String escolha =  spinner.getSelectedItem().toString();
+
+                if (escolha.equals("ADM")) {
+                    pacote.putString("Curso", "Administração");
+                    ((TextView)findViewById(R.id.test)).setText("ADM");
+
+                } else if (escolha.equals("INFO")) {
+                    pacote.putString("Curso", "Informatica");
+                    ((TextView)findViewById(R.id.test)).setText("info");
+                }
+                else {
+                    pacote.putString("Curso","ldsjfyhgas");
+                    ((TextView)findViewById(R.id.test)).setText("fasdiuhsdfjhi");
+                }
+
+            }
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+
+        });
     }
 
 
-    public void escoherSpinner(View view) {
-
-        Intent intent = new Intent(this, Main3Activity.class);
-        Bundle pacote = new Bundle();
-        Spinner theSpinner = (Spinner)findViewById(R.id.spinner1);
-        String escolha =  theSpinner.getSelectedItem().toString();
-
-        if (escolha.equals("ADM")) {
-            pacote.putString("Curso", "Administração");
-
-        } else if (escolha.equals("INFO")) {
-            pacote.putString("Curso", "Informatica");
-        }
 
 
+    public void irParaTerceiraTela (View view){
+
+         intent.putExtras(pacote);
+         startActivity(intent);
     }
 
-  public void irParaTerceiraTela (View view){
-        Intent intent = new Intent(this, Main3Activity.class);
-        startActivity(intent);
-    }
 }
--- app/src/main/java/com/example/trab_2bim/Main3Activity.java
+++ app/src/main/java/com/example/trab_2bim/Main3Activity.java
@@ -14,10 +14,14 @@ public class Main3Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main3);
 
         Intent intent = getIntent();
+
         Bundle bundle = intent.getExtras();
+
         String curso = bundle.getString("Curso");
-        TextView cursoEscolhido = findViewById(R.id.mostrar_curso);
-        cursoEscolhido.setText(curso);
+
+        TextView cursoEscolhido = (TextView) findViewById(R.id.mostrar_curso);
+
+
 
     }
 }
